1f5b8e73ff019d04bb9681fcb967cfa931886a53
findiu <727723590@qq.com>
2019-01-10 17:20:07 +0800

    commit

--- day4/Decorator_test.py
+++ day4/Decorator_test.py
@@ -1,9 +1,12 @@
 #装饰器，本质是函数（用来装饰其他函数）为其他函数添加附件功能
 #不能改被装饰函数的函数内部
 #不能改被装饰函数的调用方法
+#函数就是 变量
 import time
 def add(a,b):
     res=a+b
     print("a+b=",res)
 f=add
 f(2,4)
+clac=lambda x:x*3#匿名函数
+print(clac(3))
