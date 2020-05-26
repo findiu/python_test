f6c60c387507d6dbdeac1bb30d9f78fedc6eafa0
lwf <727723590@qq.com>
2020-05-26 14:13:50 +0800

    error

--- day1/if_test.py
+++ day1/if_test.py
@@ -1,8 +1,13 @@
-i=10
-ii=int(input("请输入一个数字i："))
-if i==ii:
-    print("i=ii")
-elif i > ii:
-    print("i>ii")
-else:
-    print("i<ii")
+import random
+print("猜数字")
+i=random.randint(0,100)
+print(i)
+ii=int(input("请输入一个数字(0~100)："))
+while i!=ii:
+    if i > ii:
+        print("你输入得数字小了")
+        ii = int(input("请重新输入一个数字(0~100)："))
+    elif i<ii:
+        print("你输入得数字大了")
+        ii = int(input("请重新输入一个数字(0~100)："))
+print("恭喜你，答对了")
